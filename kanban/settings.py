c659baa968caa8f16b987b3539f5cef1e0268c81
Lautaro Reche <lautaroreche1@gmail.com>
2025-08-07 13:45:44 +0100

    Implement tailwind cli and remove cdn import

--- kanban/settings.py
+++ kanban/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'kanban_app/static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
